09bde236ef3a31507c933a0a547e5892584a51e0
EstebanMarin <estebanmarin002@gmail.com>
2024-11-23 22:45:18 -0500

    compiling not passing test

--- modules/server/src/test/scala/com/rockthejvm/reviewboard/CompanyControllerSpec.scala
+++ modules/server/src/test/scala/com/rockthejvm/reviewboard/CompanyControllerSpec.scala
@@ -1,13 +1,44 @@
 package com.rockthejvm.reviewboard
 
+import cats.MonadError
+import com.rockthejvm.reviewboard.domain.data.Company
+import com.rockthejvm.reviewboard.http.controllers.CompanyControllers
+import com.rockthejvm.reviewboard.http.requests.CreateCompanyRequest
+import sttp.client3.*
+import sttp.client3.testing.SttpBackendStub
+import sttp.tapir.Tapir
+import sttp.tapir.server.stub.TapirStubInterpreter
+import sttp.tapir.ztapir.RIOMonadError
 import zio.*
 import zio.test.*
+import zio.test.Assertion.*
 
 object CompanyControllerSpec extends ZIOSpecDefault {
+
+  private given zioME: RIOMonadError[Any] = new RIOMonadError[Any]
   def spec = suite("CompanyController")(
-    test("should return a list of companies") {
-      assertZIO(ZIO.succeed(1 + 1))(
-        Assertion.assertion("Basic Math")(_ == 2)
+    test("should create a company successfully") {
+      val request = CreateCompanyRequest("Test Company", "test@example.com")
+      val createCompany = for {
+        controller <- CompanyControllers.makeZIO
+        backendStub = SttpBackendStub(zioME)
+        interpreter = TapirStubInterpreter(backendStub)
+        response <- ZIO.succeed(
+          interpreter
+            .whenServerEndpoint(controller.create)
+            //           .thenRespond(Company(1, "Test Company", "test-company", Some("test@example.com")))
+            //   .send(request)
+        )
+        //   .thenRespond(Company(1, "Test Company", "test-company", Some("test@example.com")))
+        //   .send(request)
+      } yield response
+      // create the controller
+      // build the tapir backend
+      // run http request
+      // check the response
+
+      assertZIO(createCompany)(
+        equalTo(Right(Company(1, "Test Company", "test-company", Some("test@example.com"))))
       )
     }
   )
